38b01cd94b9b5490c6e7063fdcb246088986dcee
Bbeniz <165205305+Bbeniz@users.noreply.github.com>
2024-05-29 20:41:10 +0100

    Update callad.js

--- scripts/cmds/callad.js
+++ scripts/cmds/callad.js
@@ -24,12 +24,12 @@ module.exports = {
 			missingMessage: "Vui lòng nhập tin nhắn bạn muốn gửi về admin",
 			sendByGroup: "\n- Được gửi từ nhóm: %1\n- Thread ID: %2",
 			sendByUser: "\n- Được gửi từ người dùng",
-			content: "\n\nNội dung:\n─────────────────\n%1\n─────────────────\nPhản hồi tin nhắn này để gửi tin nhắn về người dùng",
+			content: "\n\nNội dung:\n✇═❦═•| ۞ |•═❦═✇\n%1\n─✇═❦═•| ༄ |•═❦═✇\nPhản hồi tin nhắn này để gửi tin nhắn về người dùng",
 			success: "Đã gửi tin nhắn của bạn về %1 admin thành công!\n%2",
 			failed: "Đã có lỗi xảy ra khi gửi tin nhắn của bạn về %1 admin\n%2\nKiểm tra console để biết thêm chi tiết",
-			reply: "📍 Phản hồi từ admin %1:\n─────────────────\n%2\n─────────────────\nPhản hồi tin nhắn này để tiếp tục gửi tin nhắn về admin",
+			reply: "📍 Phản hồi từ admin %1:\n✇═❦═•| ☆ |•═❦═✇\n%2\n✇═❦═•| ✵ |•═❦═✇\nPhản hồi tin nhắn này để tiếp tục gửi tin nhắn về admin",
 			replySuccess: "Đã gửi phản hồi của bạn về admin thành công!",
-			feedback: "📝 Phản hồi từ người dùng %1:\n- User ID: %2%3\n\nNội dung:\n─────────────────\n%4\n─────────────────\nPhản hồi tin nhắn này để gửi tin nhắn về người dùng",
+			feedback: "📝 Phản hồi từ người dùng %1:\n- User ID: %2%3\n\nNội dung:\n✇═❦═•| ☢ |•═❦═✇\n%4\n✇═❦═•| ☢ |•═❦═✇\nPhản hồi tin nhắn này để gửi tin nhắn về người dùng",
 			replyUserSuccess: "Đã gửi phản hồi của bạn về người dùng thành công!",
 			noAdmin: "Hiện tại bot chưa có admin nào"
 		},
@@ -37,12 +37,12 @@ module.exports = {
 			missingMessage: "Please enter the message you want to send to admin",
 			sendByGroup: "\n- Sent from group: %1\n- Thread ID: %2",
 			sendByUser: "\n- Sent from user",
-			content: "\n\nContent:\n─────────────────\n%1\n─────────────────\nReply this message to send message to user",
+			content: "\n\nContent:\n✇═❦═•| ☼ |•═❦═✇\n%1\n✇═❦═•| ☼ |•═❦═✇\nReply this message to send message to user",
 			success: "Sent your message to %1 admin successfully!\n%2",
 			failed: "An error occurred while sending your message to %1 admin\n%2\nCheck console for more details",
-			reply: "📍 Reply from admin %1:\n─────────────────\n%2\n─────────────────\nReply this message to continue send message to admin",
+			reply: "✰ 𝐦𝐞𝐬𝐬𝐚𝐠𝐞 𝐝𝐞 𝐥'𝐚𝐝𝐦𝐢𝐧 %1:\n✇═❦═•| ✰ |•═❦═✇\n%2\n✇═❦═•| ✰ |•═❦═✇\nReply this message to continue send message to admin",
 			replySuccess: "Sent your reply to admin successfully!",
-			feedback: "📝 Feedback from user %1:\n- User ID: %2%3\n\nContent:\n─────────────────\n%4\n─────────────────\nReply this message to send message to user",
+			feedback: "📝 Feedback from user %1:\n- User ID: %2%3\n\nContent:\n✇═❦═•| ☢ |•═❦═✇\n%4\n✇═❦═•| ☢ |•═❦═✇\nReply this message to send message to user",
 			replyUserSuccess: "Sent your reply to user successfully!",
 			noAdmin: "Bot has no admin at the moment"
 		}
@@ -56,7 +56,7 @@ module.exports = {
 		if (config.adminBot.length == 0)
 			return message.reply(getLang("noAdmin"));
 		const senderName = await usersData.getName(senderID);
-		const msg = "==📨️ CALL ADMIN 📨️=="
+		const msg = "==📨 CALL ADMIN 📨=="
 			+ `\n- User Name: ${senderName}`
 			+ `\n- User ID: ${senderID}`
 			+ (isGroup ? getLang("sendByGroup", (await threadsData.get(threadID)).threadName, threadID) : getLang("sendByUser"));
