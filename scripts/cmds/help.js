addfaebb9a8e9973fe1a15b709ff94c231b374e1
Bbeniz <165205305+Bbeniz@users.noreply.github.com>
2024-05-29 20:38:47 +0100

    Update help.js

--- scripts/cmds/help.js
+++ scripts/cmds/help.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const path = require("path");
 const { getPrefix } = global.utils;
 const { commands, aliases } = global.GoatBot;
-const doNotDelete = "[ 🧜‍🌪 | ❤‍🔥𝐦𝐚𝐝𝐚𝐫𝐬☠️]";
+const doNotDelete = "[ ☢ | ❤‍🔥𝐦𝐚𝐝𝐚𝐫𝐬☠️]";
 /** 
 * @author NTKhang
 * @author: do not delete it
