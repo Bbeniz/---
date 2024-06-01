a21b07c72c0252e848401bd11bb5d80a1dc8f5d8
Bbeniz <165205305+Bbeniz@users.noreply.github.com>
2024-06-01 22:23:48 +0100

    Update sicbo.js

--- scripts/cmds/sicbo.js
+++ scripts/cmds/sicbo.js
@@ -1,59 +1,91 @@
 module.exports = {
-  config: {
-    name: "sicbo",
-    aliases: ["sic"],
-    version: "1.0",
-    author: "L𝐞 𝐯𝐢𝐝𝐞",
-    countDown: 10,
-    role: 0,
-    shortDescription: "Play Sicbo, the oldest gambling game",
-    longDescription: "Play Sicbo, the oldest gambling game, and earn money",
-    category: "game",
-    guide: "{pn} <Small/Big> <amount of money>"
-  },
+	config: {
+		name: "balance",
+		aliases: ["bal"],
+		version: "1.2",
+		author: "NTKhang",
+		countDown: 5,
+		role: 0,
+		description: {
+			vi: "xem số tiền hiện có của bạn hoặc người được tag",
+			en: "🆅🄾🅸🆁 🄻'🄰🆁🄶🅴🄽🅃 🅳🅴 🄻🄰 🄿🅴🆁🅂🄾🄽🄽🅴 🅃🄰🄶🆄é🅴 "
+		},
+		category: "economy",
+		guide: {
+			vi: "   {pn}: xem số tiền của bạn"
+				+ "\n   {pn} <@tag>: xem số tiền của người được tag",
+			en: "   {pn}: view your money"
+				+ "\n   {pn} <@tag>:✰  𝐯𝐨𝐢𝐫 𝐥'𝐚𝐫𝐠𝐞𝐧𝐭 𝐝𝐞 𝐥𝐚 𝐩𝐞𝐫𝐬𝐨𝐧𝐧𝐞 𝐭𝐚𝐠𝐮é𝐞"
+		}
+	},
 
-  onStart: async function ({ args, message, usersData, event }) {
-    const betType = args[0];
-    const betAmount = parseInt(args[1]);
-    const user = event.senderID;
-    const userData = await usersData.get(event.senderID);
+	langs: {
+		vi: {
+			money: "Bạn đang có %1$",
+			moneyOf: "%1 đang có %2$"
+		},
+		en: {
+			money: "༆ 𝐕𝐨𝐮𝐬 𝐚𝐯𝐞𝐳 《%1$💰🤑💶》",
+			《 𝙡'𝙖𝙧𝙜𝙚𝙣𝙩 𝙙𝙚 : "%1 has %2$"
+		}
+	},
 
-    if (!["small", "big"].includes(betType)) {
-      return message.reply("😼 | Choose 'small' or 'big'.");
-    }
+	onStart: async function ({ message, usersData, event, getLang }) {
+		if (Object.keys(event.mentions).length > 0) {
+			const uids = Object.keys(event.mentions);
+			let msg = "";
+			for (const uid of uids) {
+				const userMoney = await usersData.get(uid, "money");
+				msg += getLang("moneyOf", event.mentions[uid].replace("@", ""), userMoney) + '\n';
+			}
+			return message.reply(msg);
+		}
+		const userData = await usersData.get(event.senderID);
+		message.reply(getLang("money", userData.money));
+	}
+};%cmd install bal.js module.exports = {
+	config: {
+		name: "balance",
+		aliases: ["bal"],
+		version: "1.2",
+		author: "NTKhang",
+		countDown: 5,
+		role: 0,
+		description: {
+			vi: "xem số tiền hiện có của bạn hoặc người được tag",
+			en: "🆅🄾🅸🆁 🄻'🄰🆁🄶🅴🄽🅃 🅳🅴 🄻🄰 🄿🅴🆁🅂🄾🄽🄽🅴 🅃🄰🄶🆄é🅴 "
+		},
+		category: "economy",
+		guide: {
+			vi: "   {pn}: xem số tiền của bạn"
+				+ "\n   {pn} <@tag>: xem số tiền của người được tag",
+			en: "   {pn}: view your money"
+				+ "\n   {pn} <@tag>:✰  𝐯𝐨𝐢𝐫 𝐥'𝐚𝐫𝐠𝐞𝐧𝐭 𝐝𝐞 𝐥𝐚 𝐩𝐞𝐫𝐬𝐨𝐧𝐧𝐞 𝐭𝐚𝐠𝐮é𝐞"
+		}
+	},
 
-    if (!Number.isInteger(betAmount) || betAmount < 50) {
-      return message.reply("❌ | Please bet an amount of 50 or more.");
-    }
+	langs: {
+		vi: {
+			money: "Bạn đang có %1$",
+			moneyOf: "%1 đang có %2$"
+		},
+		en: {
+			money: "༆ 𝐕𝐨𝐮𝐬 𝐚𝐯𝐞𝐳《%1$💰💶》",
+			《 𝙡'𝙖𝙧𝙜𝙚𝙣𝙩 𝙙𝙚 : "%1 has %2$"
+		}
+	},
 
-    if (betAmount > userData.money) {
-      return message.reply("🖕 | Merde 🙄 t'es trop pauvre mandiant .");
-    }
-
-    const dice = [1, 2, 3, 4, 5, 6];
-    const results = [];
-
-    for (let i = 0; i < 3; i++) {
-      const result = dice[Math.floor(Math.random() * dice.length)];
-      results.push(result);
-    }
-
-    const winConditions = {
-      small: results.filter((num, index, arr) => num >= 1 && num <= 3 && arr.indexOf(num) !== index).length > 0,
-      big: results.filter((num, index, arr) => num >= 4 && num <= 6 && arr.indexOf(num) !== index).length > 0,
-    };
-
-    const resultString = results.join(" | ");
-
-    if ((winConditions[betType] && Math.random() <= 0.4) || (!winConditions[betType] && Math.random() > 0.4)) {
-      const winAmount = 2 * betAmount;
-      userData.money += winAmount;
-      await usersData.set(event.senderID, userData);
-      return message.reply(`(\/)\ •_•)\/ >[ ${resultString} ]\🎉 | Congratulations! You won ${winAmount}!`);
-    } else {
-      userData.money -= betAmount;
-      await usersData.set(event.senderID, userData);
-      return message.reply(`(\/)\ •_•)\/ >[ ${resultString} ]\😿 | You lost ${betAmount}.`);
-    }
-  }
+	onStart: async function ({ message, usersData, event, getLang }) {
+		if (Object.keys(event.mentions).length > 0) {
+			const uids = Object.keys(event.mentions);
+			let msg = "";
+			for (const uid of uids) {
+				const userMoney = await usersData.get(uid, "money");
+				msg += getLang("moneyOf", event.mentions[uid].replace("@", ""), userMoney) + '\n';
+			}
+			return message.reply(msg);
+		}
+		const userData = await usersData.get(event.senderID);
+		message.reply(getLang("money", userData.money));
+	}
 };
