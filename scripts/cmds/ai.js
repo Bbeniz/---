001db6d28dfd6659a0884b357812b2230cefcd75
Bbeniz <165205305+Bbeniz@users.noreply.github.com>
2024-06-03 13:12:48 +0100

    Update ai.js

--- scripts/cmds/ai.js
+++ scripts/cmds/ai.js
@@ -1,93 +1,82 @@
 const axios = require('axios');
 
+async function fetchFromAI(url, params) {
+ try {
+ const response = await axios.get(url, { params });
+ return response.data;
+ } catch (error) {
+ console.error(error);
+ return null;
+ }
+}
 
+async function getAIResponse(input, userId, messageID) {
+ const services = [
+ { url: 'https://ai-tools.replit.app/gpt', params: { prompt: input, uid: userId } },
+ { url: 'https://openaikey-x20f.onrender.com/api', params: { prompt: input } },
+ { url: 'http://fi1.bot-hosting.net:6518/gpt', params: { query: input } },
+ { url: 'https://ai-chat-gpt-4-lite.onrender.com/api/hercai', params: { question: input } }
+ ];
 
-const Prefixes = [
+ let response = "━━━━━━━━━━━━━ \n  ✰𝐬𝐚𝐥𝐮𝐭 𝐦𝐨𝐢 𝐜'𝐞𝐬𝐭 𝐦𝐚𝐝𝐚𝐫𝐚 𝐮𝐜𝐡𝐢𝐡𝐚 𝐭𝐮 𝐩𝐞𝐮𝐭 𝐦𝐞 𝐩𝐨𝐬𝐞𝐫 𝐭𝐚 𝐪𝐮𝐞𝐬𝐭𝐢𝐨𝐧 ☢\n━━━━━━━━━━━━━";
+ let currentIndex = 0;
 
-  'ask',
+ for (let i = 0; i < services.length; i++) {
+ const service = services[currentIndex];
+ const data = await fetchFromAI(service.url, service.params);
+ if (data && (data.gpt4 || data.reply || data.response)) {
+ response = data.gpt4 || data.reply || data.response;
+ break;
+ }
+ currentIndex = (currentIndex + 1) % services.length; // Move to the next service in the cycle
+ }
 
-  'madara',
-
-  'ai',
-
-  'bot',
-
-  'uchiha',
-
-];
-
-
+ return { response, messageID };
+}
 
 module.exports = {
+ config: {
+ name: 'ai',
+ author: 'Arn',
+ role: 0,
+ category: 'ai',
+ shortDescription: 'ai to ask anything',
+ },
+ onStart: async function ({ api, event, arns }) {
+ const input = args.join(' ').trim();
+ if (!input) {
+ api.sendMessage(``, event.threadID, event.messageID);
+ return;
+ }
 
-  config: {
+const fonts = {
 
-    name: "chatgpt",
+ mathsans: {
 
-    version: 1.0,
+ a: "𝖺", b: "𝖻", c: "𝖼", d: "𝖽", e: "𝖾", f: "𝖿", g: "𝗀", h: "𝗁", i: "𝗂",
 
-    author: "☢",
+ j: "𝗃", k: "𝗄", l: "𝗅", m: "𝗆", n: "𝗇", o: "𝗈", p: "𝗉", q: "𝗊", r: "𝗋",
 
-    role: 0,
+ s: "𝗌", t: "𝗍", u: "𝗎", v: "𝗏", w: "𝗐", x: "𝗑", y: "𝗒", z: "𝗓",
 
-    shortDescription: "Ask question to ChatGPT",
+ A: "𝖠", B: "𝖡", C: "𝖢", D: "𝖣", E: "𝖤", F: "𝖥", G: "𝖦", H: "𝖧", I: "𝖨",
 
-    longDescription: "Interact as ChatGPT provided by OpenAi. This command allows users to interact with the AI, asking various questions and receiving detailed answers.",
+ J: "𝖩", K: "𝖪", L: "𝖫", M: "𝖬", N: "𝖭", O: "𝖮", P: "𝖯", Q: "𝖰", R: "𝖱",
 
-    category: "ai",
+ S: "𝖲", T: "𝖳", U: "𝖴", V: "𝖵", W: "𝖶", X: "𝖷", Y: "𝖸", Z: "𝖹",
+ }
+};
 
-    guide: {
-
-      en: "{p}ai [ question ] - Replace '{p}' with your command prefix and 'question' with your actual query.",
-
-    },
-
-  },
-
-  
-
-  onStart: async function () {},
-
-  onChat: async function ({ api, event, args, message }) {
-
-    try {
-
-      const prefix = Prefixes.find((p) => event.body && event.body.toLowerCase().startsWith(p));
-
-      if (!prefix) {
-
-        return; // Invalid prefix, ignore the command
-
-      }
-
-      const prompt = event.body.substring(prefix.length).trim();
-
-      if (!prompt) {
-
-        await message.reply("✰𝐬𝐚𝐥𝐮𝐭 𝐦𝐨𝐢 𝐜'𝐞𝐬𝐭 𝐦𝐚𝐝𝐚𝐫𝐚 𝐮𝐜𝐡𝐢𝐡𝐚 𝐭𝐮 𝐩𝐞𝐮𝐭 𝐦𝐞 𝐩𝐨𝐬𝐞𝐫 𝐭𝐚 𝐪𝐮𝐞𝐬𝐭𝐢𝐨𝐧 ☢ ");
-
-        return;
-
-      }
-
-      api.setMessageReaction("⏰", event.messageID, (err) => {}, true);
-
-      const response = await axios.get(`https://himachalwale.onrender.com/api/chatgpt?prompt=${encodeURIComponent(prompt)}&apikey=©himachalwale`);
-
-      const answer = response.data.fullResponse;
-
-      await message.reply(answer);
-
-      api.setMessageReaction("✅", event.messageID, (err) => {}, true);
-
-    } catch (error) {
-
-      console.error("Error:", error.message, error.response?.data);
-
-      api.setMessageReaction("❌", event.messageID, (err) => {}, true);
-
-    }
-
-  }
-
+ 
+ const { response, messageID } = await getAIResponse(input, event.senderID, event.messageID);
+ api.sendMessage(` ${response} `, event.threadID, messageID);
+ },
+ onChat: async function ({ event, message }) {
+ const messageContent = event.body.trim().toLowerCase();
+ if (messageContent.startsWith("ai")) {
+ const input = messageContent.replace(/^ai\s*/, "").trim();
+ const { response, messageID } = await getAIResponse(input, event.senderID, message.messageID);
+ message.reply(`༆𝐌𝐚𝐝𝐚𝐫𝐚 𝐮𝐜𝐡𝐢𝐡𝐚\n━━━━━━━━━━━━━\n${response}`, messageID);
+ }
+ }
 };
