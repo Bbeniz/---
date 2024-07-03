92159ddc9320aa146634fa26150823680b545728
Bbeniz <165205305+Bbeniz@users.noreply.github.com>
2024-07-03 19:55:53 +0100

    Update noti.js

--- scripts/cmds/noti.js
+++ scripts/cmds/noti.js
@@ -31,7 +31,7 @@ module.exports = {
 		},
 		en: {
 			missingMessage: "Please enter the message you want to send to all groups",
-			notification: "◆❯━▣\n📬𝐋𝐄 𝐂𝐇𝐄𝐅 𝐃𝐔 𝐂𝐋𝐀𝐍 𝐕𝐎𝐔𝐒 𝐀 𝐄𝐍𝐕𝐎𝐘𝐄𝐑 𝐔𝐍 𝐌𝐄𝐒𝐒𝐀𝐆𝐄📬▣━❮◆",
+			notification: "◆❯━━━━━━━▣✦▣━━━━━━━━❮◆📬𝐋𝐄 𝐂𝐇𝐄𝐅 𝐃𝐔 𝐂𝐋𝐀𝐍 𝐕𝐎𝐔𝐒 𝐀 𝐄𝐍𝐕𝐎𝐘𝐄𝐑 𝐔𝐍 𝐌𝐄𝐒𝐒𝐀𝐆𝐄📬",
 			sendingNotification: "Start sending notification from admin bot to %1 chat groups",
 			sentNotification: "✅ Sent notification to %13 groups successfully",
 			errorSendingNotification: "An error occurred while sending to %1 groups:\n%2"
